0b82eb1ab27261e67550b9ae8ab2c92b0db83100
Oshiokhaiyamhe <oshio.choco1@gmail.com>
2023-12-13 07:41:54 -0800

    added arguments

--- exec_command.c
+++ exec_command.c
@@ -1,6 +1,6 @@
 #include "mshell.h"
 
-void exec_command(const char *prompt_command) {
+void exec_command(const char *prompt_command, char *arguments[]) {
 	pid_t c_pid = fork(); /*this is to create child process*/
 	
 	if (c_pid == -1) {
@@ -8,13 +8,14 @@ void exec_command(const char *prompt_command) {
 		exit(EXIT_FAILURE);
 	} else if (c_pid == 0) {
 		/*
-		 * all child process
+		 * all child process continues
 		 */
-		execlp(prompt_command, prompt_command, (char *)NULL);
-		perror("execlp");
+		/**execlp(prompt_command, prompt_command, (char *)NULL);**/
+		execvp(prompt_command, arguments);
+		perror("Command Execution failed");
 		exit(EXIT_FAILURE);
 	} else {
-		/*parent process*/
+		/*parent process pauses a little and continues shortly after the child process is completed*/
 		wait(NULL);
 	}
 }
--- mshell.h
+++ mshell.h
@@ -10,9 +10,13 @@
 #include <string.h>
 #include <sys/wait.h>
 
+#define MAX_ARGS 64
+#define MAX_INPUT_SIZE 1024
 
 void print_prompt(void);
 void m_print(const char *string_prompt);
 void read_cmd(char *prompt_command, size_t prompt_size);
-void exec_command(const char *prompt_command);
+void exec_command(const char *prompt_command, char *arguments[]);
+void tokenize_input(char *prompt_command, char *arguments[]);
+
 #endif /* MSHELL_H */
